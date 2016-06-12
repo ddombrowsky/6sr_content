5221be80b786fa471aee5a85c9eb1932f4a91653
David Dombrowsky <davek@6thstreetradio.org>
2016-06-12 01:05:09 -0400

    segments and content raw data now shows in admin page

--- django/sixsr/sixsr_web/admin.py
+++ django/sixsr/sixsr_web/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Segments, Content;
+
+admin.site.register(Segments);
+admin.site.register(Content);
--- django/sixsr/sixsr_web/models.py
+++ django/sixsr/sixsr_web/models.py
@@ -1,12 +1,5 @@
-# This is an auto-generated Django model module.
-# You'll have to do the following manually to clean this up:
-#   * Rearrange models' order
-#   * Make sure each model has one field with primary_key=True
-#   * Remove `managed = False` lines for those models you wish to give write DB access
-# Feel free to rename the models, but don't rename db_table values or field names.
 #
-# Also note: You'll have to insert the output of 'django-admin.py sqlcustom [appname]'
-# into your database.
+#
 from __future__ import unicode_literals
 
 from django.db import models
@@ -33,4 +26,5 @@ class Segments(models.Model):
 		db_table = 't$segments'
 
 	def __str__(self):
-		return str(self.start_time) + ' -> ' + str(self.end_time);
+		return (str(self.start_time) + ' -> ' + str(self.end_time) +
+		       ' priority:' + str(self.priority));
